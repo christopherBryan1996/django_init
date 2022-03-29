d4d79e7b390de2752c209ca53379c39fe6ef8d97
bryan mancilla <mancillabryan1996@outlook.com>
2022-03-28 18:03:22 -0600

    end servicios

--- Proyectoweb/ProyectoWebApp/urls.py
+++ Proyectoweb/ProyectoWebApp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from ProyectoWebApp.views import home,servicios,tienda,blog,contacto
+from ProyectoWebApp.views import home, tienda,blog,contacto
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('',home,name='Home'),
-    path('servicios/',servicios,name='Servicios'),
     path('tienda/',tienda,name='Tienda'),
     path('blog/',blog,name='Blog'),
     path('contacto/',contacto,name='Contacto'),
--- Proyectoweb/ProyectoWebApp/views.py
+++ Proyectoweb/ProyectoWebApp/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(req):
     return render(req,'ProyectoWebApp/home.html')
-
-def servicios(req):
-    return render(req,'ProyectoWebApp/servicios.html')
 
 def tienda(req):
     return render(req,'ProyectoWebApp/tienda.html')
